356d4b0212020577a124392346eb106dc8601a47
Serhii-Vivdenko <Sergejarty@gmail.com>
2024-02-23 18:39:22 +0300

    commit18

---
+++ src/main/java/com/doctor/appointment/config/H2BeanConfig.java
@@ -0,0 +1,17 @@
+package com.doctor.appointment.config;
+
+//import org.apache.catalina.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.h2.tools.Server;
+import java.sql.SQLException;
+
+@Configuration
+public class H2BeanConfig {
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseaServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
+
+}
