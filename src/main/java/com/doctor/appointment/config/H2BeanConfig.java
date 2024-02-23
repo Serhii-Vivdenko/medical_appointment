c631dc28dc83c57142ed22cab1ee728dd4eee82c
Serhii-Vivdenko <Sergejarty@gmail.com>
2024-02-23 22:58:50 +0300

    commit19

--- src/main/java/com/doctor/appointment/config/H2BeanConfig.java
+++ src/main/java/com/doctor/appointment/config/H2BeanConfig.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.h2.tools.Server;
 import java.sql.SQLException;
 
-@Configuration
+//@Configuration
 public class H2BeanConfig {
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseaServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
-    }
+//    @Bean(initMethod = "start", destroyMethod = "stop")
+//    public Server inMemoryH2DatabaseaServer() throws SQLException {
+//        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+//    }
 
 }
--- src/test/java/com/doctor/appointment/AppointmentImplTest.java
+++ src/test/java/com/doctor/appointment/AppointmentImplTest.java
@@ -1,7 +1,10 @@
 package com.doctor.appointment;
 
+import com.doctor.appointment.dto.appointment.CreateRequestAppointmentDto;
 import com.doctor.appointment.model.Appointment;
+import com.doctor.appointment.model.Doctor;
 import com.doctor.appointment.repository.AppointmentRepository;
+import com.doctor.appointment.service.AppointmentService;
 import com.doctor.appointment.service.impl.AppointmentImpl;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,24 +22,53 @@ import java.time.LocalDateTime;
 
 //@ExtendWith(MockitoExtension.class)
 @SpringBootTest
-//@Transactional
 public class AppointmentImplTest {
-//    @Mock
     @Autowired
     private  AppointmentRepository appointmentRepository;
-    private AppointmentImpl appointmentImpl;
+    @Autowired
+    private AppointmentService appointmentService;
+//    private AppointmentImpl appointmentImpl;
     @BeforeEach
     void setUp() {
-        appointmentImpl = new AppointmentImpl(appointmentRepository);
+        appointmentService = new AppointmentImpl(appointmentRepository);
     }
 
     @Test
-    public void updateAppointment() {
-        Appointment updated = appointmentImpl.readById(1);
-        updated.setStartDateTime(LocalDateTime.parse("2024-02-20T09:00:00"));
-        appointmentImpl.update(updated);
-        Appointment result = appointmentImpl.readById(1);
-        Assertions.assertEquals(LocalDateTime.parse("2024-02-20T09:00:00"), result.getStartDateTime());
+    public void read() {
+        Appointment appointment = appointmentService.readById(1);
+        Assertions.assertEquals(1,appointment.getId());
 
     }
+
+    @Test
+    public void createAppointment() {
+        CreateRequestAppointmentDto dto = new CreateRequestAppointmentDto();
+        dto.setStartDateTime(LocalDateTime.parse("2024-01-20T07:30:00"));
+        dto.setEndDateTime(LocalDateTime.parse("2024-01-20T08:30:00"));
+        dto.setDoctorId(2L);
+        Appointment created = appointmentService.create(dto);
+        Appointment appointment = appointmentService.readById(6);
+        Assertions.assertEquals(LocalDateTime.parse("2024-01-20T07:30:00"),created.getStartDateTime());
+        Assertions.assertEquals(6,appointment.getId());
+
+    }
+    @Test
+    public void updateAppointment() {
+        Appointment updated = appointmentService.readById(2);
+        updated.setStartDateTime(LocalDateTime.parse("2024-01-20T11:30:00"));
+        appointmentService.update(updated);
+        Appointment result = appointmentService.readById(2);
+        Assertions.assertEquals(LocalDateTime.parse("2024-01-20T11:30:00"), result.getStartDateTime());
+    }
+
 }
+
+//@ExtendWith(MockitoExtension.class)
+//public class AppointmentImplTest {
+//    @Mock
+//    private AppointmentRepository appointmentRepository;
+//    @InjectMocks
+//    private AppointmentImpl appointmentImpl;
+//}
+
+
