eed06de88c7f547ff9787047d31e4d739318c23e
Serhii-Vivdenko <Sergejarty@gmail.com>
2024-03-01 23:24:04 +0300

    commit33

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -64,11 +64,38 @@ CREATE TABLE placeOfWorkDoctors
     placeOfWork_id bigint NOT NULL,
     doctor_id bigint NOT NULL,
     CONSTRAINT fk_placeOfWork FOREIGN KEY (placeOfWork_id)
-        REFERENCES Locations (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+        REFERENCES locations (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT fk_doctor FOREIGN KEY (doctor_id)
-        REFERENCES Doctors (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+        REFERENCES doctors (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 INSERT INTO placeOfWorkDoctors (placeOfWork_id, doctor_id)
 VALUES (1,1),
-       (2,2)
+       (2,2);
+
+CREATE TABLE specializations
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+    specialization character varying(150) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO specializations (specialization)
+VALUES ('surgeon'),
+       ('therapist'),
+       ('therapist');
+
+CREATE TABLE specializationsAndDoctors
+(
+    Specialization_id bigint NOT NULL,
+    doctor_id bigint NOT NULL,
+    CONSTRAINT fk_specializationsAndDoctors_specialization_id FOREIGN KEY (Specialization_id)
+        REFERENCES specializations (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT fk_specializationsAndDoctors_doctor_id FOREIGN KEY (doctor_id)
+        REFERENCES Doctors (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+INSERT INTO specializationsAndDoctors (Specialization_id, doctor_id)
+VALUES (1,1),
+       (2,2),
+       (3,1);
